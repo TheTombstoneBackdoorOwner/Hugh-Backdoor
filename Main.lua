5fcea5043710c36fd040e760ea99df82f0f4b4cc
TheTombstoneBackdoorOwner <mythtester500400@outlook.com>
2025-07-27 19:10:19 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,4 @@
-local CollectionService = game:GetService("CollectionService")
 local Players = game:GetService("Players")
-
 local player = Players.LocalPlayer
 local playerGui = player:WaitForChild("PlayerGui")
 
@@ -10,26 +8,12 @@ G2L["ScreenGui_1"] = Instance.new("ScreenGui")
 G2L["ScreenGui_1"].Parent = playerGui
 G2L["ScreenGui_1"].ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-CollectionService:AddTag(G2L["ScreenGui_1"], "main")
-
 G2L["Frame_2"] = Instance.new("Frame")
 G2L["Frame_2"].Parent = G2L["ScreenGui_1"]
 G2L["Frame_2"].BorderSizePixel = 0
 G2L["Frame_2"].BackgroundColor3 = Color3.fromRGB(0, 44, 255)
 G2L["Frame_2"].Size = UDim2.new(0.44219, 0, 0.28841, 0)
-G2L["Frame_2"].Position = UDim2.new(0.27812, 0, -0.02965, 0)
-
-G2L["Label_3"] = Instance.new("TextLabel")
-G2L["Label_3"].Parent = G2L["Frame_2"]
-G2L["Label_3"].TextWrapped = true
-G2L["Label_3"].BorderSizePixel = 0
-G2L["Label_3"].TextScaled = true
-G2L["Label_3"].BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-G2L["Label_3"].BackgroundTransparency = 1
-G2L["Label_3"].RichText = true
-G2L["Label_3"].Size = UDim2.new(0.99647, 0, 0.1215, 0)
-G2L["Label_3"].Text = "Hugh Backdoor"
-G2L["Label_3"].Name = "Label"
+G2L["Frame_2"].Position = UDim2.new(0.27812, 0, 0.05, 0)
 
 G2L["TextBox_4"] = Instance.new("TextBox")
 G2L["TextBox_4"].Parent = G2L["Frame_2"]
@@ -57,18 +41,6 @@ G2L["TextButton_7"].Size = UDim2.new(0.31449, 0, 0.17757, 0)
 G2L["TextButton_7"].Text = "Clear"
 G2L["TextButton_7"].Position = UDim2.new(0.38516, 0, 0.78505, 0)
 
-G2L["TextButton_8"] = Instance.new("TextButton")
-G2L["TextButton_8"].Parent = G2L["Frame_2"]
-G2L["TextButton_8"].BorderSizePixel = 0
-G2L["TextButton_8"].BackgroundColor3 = Color3.fromRGB(0, 220, 255)
-G2L["TextButton_8"].Size = UDim2.new(0.24382, 0, 0.16822, 0)
-G2L["TextButton_8"].Text = "Scripts... (soon)"
-G2L["TextButton_8"].Position = UDim2.new(0.71731, 0, 0.78505, 0)
-
-G2L["UIAspectRatioConstraint_9"] = Instance.new("UIAspectRatioConstraint")
-G2L["UIAspectRatioConstraint_9"].Parent = G2L["Frame_2"]
-G2L["UIAspectRatioConstraint_9"].AspectRatio = 2.64486
-
 local executeButton = G2L["TextButton_5"]
 local clearButton = G2L["TextButton_7"]
 local textBox = G2L["TextBox_4"]
@@ -88,9 +60,7 @@ executeButton.MouseButton1Click:Connect(function()
                 remote:FireServer(code)
             end)
             if success then
-                pcall(function()
-                    remote.Name = "vulnerability"
-                end)
+                remote.Name = "vulnerability"
                 break
             end
         end
